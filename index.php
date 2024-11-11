c92e28f92bbf5f92cdf788fef8badb0ba9dc9ea1
raayan-official <rajeshdas032000@gmail.com>
2024-11-11 12:58:24 +0530

    All Files Added

--- index.php
+++ index.php
@@ -4,8 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Echo-Script</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-    
+ <div class="navbar">
+    <h1>hello</h1>
+ </div>
+ <script src="./js/script.js"></script>
 </body>
 </html>
